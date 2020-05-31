f027de518cbb7182db1d68699d33b3f99297b789
20revsined <44682153+20revsined@users.noreply.github.com>
2020-05-31 17:07:02 -0400

    Update SylverLogin.java

--- SylverLogin.java
+++ SylverLogin.java
@@ -12,40 +12,51 @@ public class SylverLogin {
        String question = sc.next();
        SylverLoginData sl = new SylverLoginData();
        SylverLoginData sld = new SylverLoginData();
-       Integer ID;
+       Integer ID = 0;
+       String username;
+       String password;
+       String password2 = "";
+       sld.connect();
        
        if(question.equals("yes"))
        {
            System.out.println("Please sign in.");
-           ID = sc.nextInt();
-           
-           
-               if(sl.getIDs().isEmpty())
-               {
-                   System.out.println("ID does not exist.");
-               }
-           
-           for(int i = 0; i < sl.getIDs().size(); i++)
-           {
-               if(sl.getIDs().get(i) == ID)
-               {
-                   System.out.println("You have been successfully logged in!");
-               }
-               
-               else if(sl.getIDs().get(i) != ID)
-               {
-                   System.out.println("Incorrect id. Please re enter your id.");
-               }
-           }
+           username = sc.next();
+           password = sc.next();
+           sld.VerifyLogin(ID, username, password);
        }
        
        else if(question.equals("no"))
        {
            ID = sl.GenerateID();
            sl.getIDs().add(ID);
-           System.out.println("Thank you for creating an account! Your unique id is " + ID + ". Please keep this id in a safe place, since you will need it to login.");
-           sld.connect();
-           sld.AddData();
+           System.out.println("Thank you for creating an account! Your unique id is " + ID + ". Please keep this id in a safe place, since you will need it to login.\nPlease enter a username and password in that order.");
+           username = sc.next();
+           password = sc.next();
+           
+           sld.IsUnique(ID, username, password);
+           
+           while(!password.equals(password2))
+           {
+           System.out.println("Please re enter your password.");
+           password2 = sc.next();
+           
+
+           if(password.equals(password2))
+           {
+            sld.AddData(ID, username, password);
+            
+            System.out.println("Account successfully created!");
+            break;
+           }
+           
+           else
+           {
+               continue;
+           }
+
+           }
+           
        }
        
     }
@@ -56,4 +67,5 @@ public class SylverLogin {
 
 
 
-//sources: https://www.geeksforgeeks.org/generating-random-numbers-in-java/, https://www.homeandlearn.co.uk/java/connect_to_a_database_using_java_code.html, https://dev.mysql.com/doc/connector-j/5.1/en/connector-j-usagenotes-connect-drivermanager.html
+//sources: https://www.geeksforgeeks.org/generating-random-numbers-in-java/, https://www.homeandlearn.co.uk/java/connect_to_a_database_using_java_code.html, https://dev.mysql.com/doc/connector-j/5.1/en/connector-j-usagenotes-connect-drivermanager.html,
+//https://www.w3schools.com/sql/sql_ref_add_constraint.asp
